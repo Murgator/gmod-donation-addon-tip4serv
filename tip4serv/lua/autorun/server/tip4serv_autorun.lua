fdc1d73e7a5687cf7bb73dbf57bd57ec21c1d6bb
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2023-04-25 15:53:53 +0200

    Improved spacing

--- tip4serv/lua/autorun/server/tip4serv_autorun.lua
+++ tip4serv/lua/autorun/server/tip4serv_autorun.lua
@@ -2,9 +2,9 @@
 
 include("autorun/sha256.lua")
 
---Tip4serv class
+-- Tip4serv class
 if not Tip4serv then
-    --class members
+    -- class members
 
     Tip4serv = {}
     Tip4serv.response_path = "tip4serv/response.json"
@@ -16,9 +16,9 @@ if not Tip4serv then
     }
 
     
-    --class methods
+    -- class methods
 
-    --Generates config files for tip4serv
+    -- Generates config files for tip4serv
     function Tip4serv.Config.CreateConfig() 
         if not file.Exists("tip4serv".."/config.json","DATA") then 
             file.CreateDir("tip4serv")
@@ -26,24 +26,24 @@ if not Tip4serv then
         end
     end
     
-    --Load config files for tip4serv
+    -- Load config files for tip4serv
     function Tip4serv.Config.Load()
         local data = file.Read("tip4serv".."/config.json","DATA")
         if not data then MsgC(Color(255,0,0),"Config file not found for Tip4serv\n") return end
         Tip4serv.Config.data = util.JSONToTable(data)
     end
 
-    --Main Function of the class : retrieve transactions, handle transactions & send transaction status
+    -- Main Function of the class : retrieve transactions, handle transactions & send transaction status
     Tip4serv.check_pending_commands = function (server_id,private_key,public_key,timestamp,get_cmd)
-        --MAC calculation        
+        -- MAC calculation        
         local MAC = Tip4serv.calculateHMAC(server_id, public_key, private_key, timestamp)
-        --Get last infos json file
+        -- Get last infos json file
         local response = Tip4serv_LoadResourceFile(Tip4serv.response_path)
         local json_encoded = ""
         if (response) then
             json_encoded = Tip4serv.urlencode(response)
         end
-        --Request Tip4serv
+        -- Request Tip4serv
         local statusUrl = "https://api.tip4serv.com/payments_api_v2.php?id="..server_id.."&time="..timestamp.."&json="..json_encoded.."&get_cmd="..get_cmd
           
         http.Fetch(statusUrl,function(tip4serv_response,size,headers,statusCode)
@@ -54,46 +54,45 @@ if not Tip4serv then
                 end
                 return
             end                
-            --Tip4serv connect
+            -- Tip4serv connect
             if (get_cmd == "no") then
                 MsgC(Color(0,255,0),tip4serv_response) return
             end    
-            --Check for error
+            -- Check for error
             local json_decoded = util.JSONToTable(tip4serv_response)        
             if (json_decoded == nil) then
                 if string.match(tip4serv_response, "No pending payments found") then
                     Tip4serv_SaveResourceFile(Tip4serv.response_path, "")
-                    --MsgC(Color(0,255,0),tip4serv_response) 
                     return                
                 elseif string.match(tip4serv_response, "Tip4serv") then
                     MsgC(Color(0,255,0),tip4serv_response) 
                     return
                 end    
             end
-            --Clear old json infos
+            -- Clear old json infos
             Tip4serv_SaveResourceFile(Tip4serv.response_path, "")
-            --Loop customers
+            -- Loop customers
             local new_json = {}
         
             for k,infos in ipairs(json_decoded) do
                 local new_obj = {} local new_cmds = {}
                 new_obj["date"] = os.date("%c")
                 new_obj["action"] = infos["action"]
-                --Check if player is online and get username
+                -- Check if player is online and get username
                 player_infos = Tip4serv.checkifPlayerIsLoaded(infos)
                 
                 if player_infos then
                     player_infos:PrintMessage(HUD_PRINTTALK,Tip4serv.Config.data.order_received_text)
                 end
                 
-                --Execute commands for player
+                -- Execute commands for player
                 if type(infos["cmds"]) == "table" then                  
                     for k,cmd in ipairs(infos["cmds"]) do
-                        --Do not run this command if the player must be online
+                        -- Do not run this command if the player must be online
                         if (player_infos == false and (string.match(cmd["str"], "{") or cmd["state"] == 1)) then
                             new_obj["status"] = 14
                         else
-                            --Replace option by player username
+                            -- Replace option by player username
                             if (player_infos and string.match(cmd["str"], "{gmod_username}")) then
                                 cmd["str"] = string.gsub(cmd["str"], "{gmod_username}", player_infos:Nick())
                             end
@@ -106,17 +105,17 @@ if not Tip4serv then
                     new_json[infos["id"]] = new_obj
                 end
             end
-            --Save the new json file
+            -- Save the new json file
             Tip4serv_SaveResourceFile(Tip4serv.response_path, util.TableToJSON(new_json))
         end, function(message) end, { ['Authorization'] = MAC })          
     end    
     
-    -- characters to hexadecimal (used for URL ENCODING)
+    -- Characters to hexadecimal (used for URL ENCODING)
     local char_to_hex = function(c)
       return string.format("%%%02X", string.byte(c))
     end    
     
-    --Loop over the playerlist & return a boolean if the player is connected
+    -- Loop over the playerlist & return a boolean if the player is connected
     Tip4serv.checkifPlayerIsLoaded = function ( infos)
         if infos["steamid"] ~= "" then
             for i, v in ipairs( player.GetAll() ) do
@@ -128,7 +127,7 @@ if not Tip4serv then
         return false
     end
     
-    --Base64 encoding algorithm
+    -- Base64 encoding algorithm
     Tip4serv.base64_encode = function ( data )
         local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
         return ((data:gsub('.', function(x) 
@@ -143,13 +142,13 @@ if not Tip4serv then
         end)..({ '', '==', '=' })[#data%3+1])
     end
     
-    --Decrypt the secret key
+    -- Decrypt the secret key
     Tip4serv.calculateHMAC = function (server_id, public_key, private_key, timestamp)
         local datas = server_id..public_key..timestamp
         return Tip4serv.base64_encode(sha256.hmac_sha256(private_key, datas))
     end
     
-    --URL Encoding algorithm for sending transaction data
+    -- URL Encoding algorithm for sending transaction data
     Tip4serv.urlencode = function(url)
       if url == nil then
         return
@@ -160,7 +159,7 @@ if not Tip4serv then
       return url
     end
     
-    --Execute commands on the server
+    -- Execute commands on the server
     Tip4serv.exe_command = function(cmd)        
         MsgC(Color(0,255,0),"[Tip4serv] execute command: "..cmd)
         argv_gmod =  Tip4serv.split(cmd," ")
@@ -169,7 +168,7 @@ if not Tip4serv then
         RunConsoleCommand(main_cmd,unpack(argv_gmod))
     end
     
-    --Split the differents commands to execute
+    -- Split the differents commands to execute
     Tip4serv.split =  function(inputstr, sep)
         if sep == nil then
             sep = "%s"
@@ -213,7 +212,7 @@ concommand.Add("tip4serv",function(ply,cmd,args)
 end)
 
 
---External functions
+-- External functions
 
 
 -- Checks if a purchase has been made every x minutes
@@ -222,7 +221,7 @@ function Tip4serv_checkPayment_every_x_min()
     Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),"yes")
 end
 
---Verify if the secret key is valid
+-- Verify if the secret key is valid
 function Tip4serv_check_api_key_validity()
     local missing_key = "[Tip4serv error] Please set key to a valid API key in data/tip4serv/config.json then restart tip4serv resource. Make sure you have copied the entire key on Tip4serv.com (CTRL+A then CTRL+C)"
     key_arr = {} i = 0
@@ -236,7 +235,7 @@ end
 
 -- Utils functions
 
---Load transactions files
+-- Load transactions files
 function Tip4serv_LoadResourceFile(path) 
     if not file.Exists(path,"DATA") then 
         file.CreateDir("tip4serv")
@@ -247,7 +246,7 @@ function Tip4serv_LoadResourceFile(path)
     return data
 end
 
---Save transaction files
+-- Save transaction files
 function Tip4serv_SaveResourceFile(path,data)
     file.Write(path,data)
 end
