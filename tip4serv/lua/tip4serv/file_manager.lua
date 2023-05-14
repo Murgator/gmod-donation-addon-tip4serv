4889614d28e580bdfcf95f799afeeff450fddb79
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2023-05-14 11:46:30 +0200

    Create file_manager.lua

---
+++ tip4serv/lua/tip4serv/file_manager.lua
@@ -0,0 +1,21 @@
+
+-- File Manager class : handles the I/O for the data files --
+
+if not File_manager then
+    File_manager = {}
+    -- Load transactions files
+    File_manager.load_resource_file = function(path)
+        if not file.Exists(path,"DATA") then 
+            file.CreateDir("tip4serv")
+            file.Write(path,"")
+        end
+        local data = file.Read(path,"DATA")
+        if not data then MsgC(Color(255,0,0),"Error while trying to read Tip4serv file\n") return end
+        return data
+    end
+
+    -- Save transaction files
+    File_manager.save_resource_file = function(path,data)
+        file.Write(path,data)
+    end
+end
