bee8c764de29589295125b34ae8608fff7bfb3c5
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2023-06-03 09:06:12 +0200

    Tip4serv bad config disabled

--- tip4serv/lua/tip4serv/init.lua
+++ tip4serv/lua/tip4serv/init.lua
@@ -12,26 +12,35 @@ end
 Tip4serv.Config.CreateConfig()
 Tip4serv.Config.Load()
 -- Check Tip4serv connection on script start
-timer.Simple(0,function()
-    timer.Create( "Tip4serv_CheckPaymentLoop", tonumber(Tip4serv.Config.data.request_interval_in_minutes)*60, 0, function() Tip4serv_checkPayment_every_x_min() end ) 
-    local key_arr = Tip4serv.check_api_key_validity()
-    if key_arr  == false then return end
-    Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
-end)
+if Tip4serv.enabled then
+   
+   timer.Simple(0,function()
+		   print(os.time(os.date("!*t")))
+		   timer.Create( "Tip4serv_CheckPaymentLoop", tonumber(Tip4serv.Config.data.request_interval_in_minutes)*60, 0, function() Tip4serv_checkPayment_every_x_min() end ) 
+		   local key_arr = Tip4serv.check_api_key_validity()
+		   if key_arr  == false then return end
+		   Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
+   end)
 
 
--- Tip4serv connect command
-concommand.Add("tip4serv",function(ply,cmd,args)
-    --Only allow commands directly from server
-    if IsValid(ply) then return end
-    if(args[1] == "connect") then 
-        Tip4serv.Config.CreateConfig()
-        Tip4serv.Config.Load()
-        MsgC(Tip4serv.Colors.green,"Connecting to Tip4Serv...\n")
-        local key_arr = Tip4serv.check_api_key_validity()
-        if key_arr == false then return end
-        Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
-    else  --Tip4extended
-        Tip4extended.runTip4serv(args)
-    end
-end)
+   -- Tip4serv connect command
+   concommand.Add("tip4serv",function(ply,cmd,args)
+		     --Only allow commands directly from server
+		     if IsValid(ply) then return end
+		     if(args[1] == "connect") then 
+			Tip4serv.Config.CreateConfig()
+			Tip4serv.Config.Load()
+			MsgC(Tip4serv.Colors.green,"Connecting to Tip4Serv...\n")
+			local key_arr = Tip4serv.check_api_key_validity()
+			if key_arr == false then return end
+			Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
+		     else  --Tip4extended
+			Tip4extended.runTip4serv(args)
+		     end
+   end)
+   concommand.Add("darkrp",function(ply,cmd,args)
+		     if IsValid(ply) then return end
+
+		     Tip4eaxtended.runTip4serv(args)
+   end)
+end
