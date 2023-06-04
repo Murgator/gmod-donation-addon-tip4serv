304627adceca5f0b3b1a57122c6ada3bae1128e9
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2023-06-04 21:10:09 +0200

    Removed useless alias

--- tip4serv/lua/tip4serv/init.lua
+++ tip4serv/lua/tip4serv/init.lua
@@ -15,7 +15,6 @@ end
 -- Check Tip4serv connection on script start
 if Tip4serv.enabled then
    timer.Simple(0,function()
-        print(os.time(os.date("!*t")))
         timer.Create( "Tip4serv_CheckPaymentLoop", tonumber(Tip4serv.Config.data.request_interval_in_minutes)*60, 0, function() Tip4serv_checkPayment_every_x_min() end ) 
         local key_arr = Tip4serv.check_api_key_validity()
         if key_arr  == false then return end
@@ -32,12 +31,8 @@ if Tip4serv.enabled then
             local key_arr = Tip4serv.check_api_key_validity()
             if key_arr == false then return end
                 Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
-            else  --Tip4extended
+        else  --Tip4extended
                 Tip4extended.runTip4serv(args)
-            end
-        end)
-    concommand.Add("darkrp",function(ply,cmd,args)
-        if IsValid(ply) then return end
-        Tip4eaxtended.runTip4serv(args)
+        end
     end)
 end
