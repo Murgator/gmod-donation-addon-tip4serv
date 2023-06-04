c101412fdbb4da028ca6717cc1efa3a13dc79f68
Murgator <38978895+Murgator@users.noreply.github.com>
2023-06-04 18:49:57 +0200

    Update init.lua

--- tip4serv/lua/tip4serv/init.lua
+++ tip4serv/lua/tip4serv/init.lua
@@ -1,5 +1,10 @@
 include("tip4serv/tip4serv.lua")
 include ("tip4serv/tip4extended.lua")
+
+-- Create and load config
+Tip4serv.Config.CreateConfig()
+Tip4serv.Config.Load()
+
 -- Checks if a purchase has been made every x minutes
 local function Tip4serv_checkPayment_every_x_min()
     local key_arr = Tip4serv.check_api_key_validity()
@@ -7,13 +12,9 @@ local function Tip4serv_checkPayment_every_x_min()
     Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),true)
 end
 
-
-
-Tip4serv.Config.CreateConfig()
-Tip4serv.Config.Load()
 -- Check Tip4serv connection on script start
 if Tip4serv.enabled then
-    timer.Simple(0,function()
+   timer.Simple(0,function()
         print(os.time(os.date("!*t")))
         timer.Create( "Tip4serv_CheckPaymentLoop", tonumber(Tip4serv.Config.data.request_interval_in_minutes)*60, 0, function() Tip4serv_checkPayment_every_x_min() end ) 
         local key_arr = Tip4serv.check_api_key_validity()
@@ -21,8 +22,8 @@ if Tip4serv.enabled then
         Tip4serv.check_pending_commands(key_arr[0], key_arr[1], key_arr[2], os.time(os.date("!*t")),false)
    end)
    -- Tip4serv connect command
-    concommand.Add("tip4serv",function(ply,cmd,args)
-     --Only allow commands directly from server
+   concommand.Add("tip4serv",function(ply,cmd,args)
+        --Only allow commands directly from server
         if IsValid(ply) then return end         
         if(args[1] == "connect") then 
             Tip4serv.Config.CreateConfig()
