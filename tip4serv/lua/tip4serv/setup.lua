2bf2b40d6d4fae0a2f4e569dde7dc8d86418281c
Murgator <38978895+Murgator@users.noreply.github.com>
2023-06-09 12:36:29 +0200

    Update and rename file_manager.lua to setup.lua

---
+++ tip4serv/lua/tip4serv/setup.lua
@@ -0,0 +1,12 @@
+Tip4serv = Tip4serv or {}
+
+Tip4serv.Config = Tip4serv.Config or {}
+Tip4serv.Colors = Tip4serv.Colors or {}
+Tip4serv.MessageCache = Tip4serv.MessageCache or {}
+
+Tip4serv.prefix_msgc = "[Tip4serv]"
+Tip4serv.response_path = "tip4serv/response.json"
+
+-- Color Caching
+Tip4serv.Colors.red = Color(255,0,0)
+Tip4serv.Colors.green = Color(0,255,0)
