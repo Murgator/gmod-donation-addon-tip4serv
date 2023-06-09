fc9a868014d9460c9074d2b00c6086319a47afdf
Murgator <38978895+Murgator@users.noreply.github.com>
2023-06-09 12:38:36 +0200

    Update and rename tip4serv_autorun.lua to tip4serv_initialize.lua

--- tip4serv/lua/autorun/server/tip4serv_autorun.lua
+++
@@ -1,2 +0,0 @@
--- Tip4serv plugin 1.2.4
-include("tip4serv/init.lua")
---
+++ tip4serv/lua/autorun/server/tip4serv_initialize.lua
@@ -0,0 +1,6 @@
+-- INITIALIZE SCRIPT
+include( "tip4serv/setup.lua" )
+include( "tip4serv/sha256.lua" )
+include( "tip4serv/tip4serv.lua" )
+include( "tip4serv/tip4extended.lua" )
+include( "tip4serv/init.lua" )
