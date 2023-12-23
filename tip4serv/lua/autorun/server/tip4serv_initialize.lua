eaa4f51ff6f7dfbe8ef536240edeb09df8eb5366
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2023-12-23 15:46:40 +0100

    new storage update

--- tip4serv/lua/autorun/server/tip4serv_initialize.lua
+++ tip4serv/lua/autorun/server/tip4serv_initialize.lua
@@ -1,6 +1,8 @@
 -- INITIALIZE SCRIPT
 include( "tip4serv/setup.lua" )
 include( "tip4serv/sha256.lua" )
+include("tip4serv/tip4mysql.lua")
+include("tip4serv/tip4storage.lua")
 include( "tip4serv/tip4serv.lua" )
 include( "tip4serv/tip4extended.lua" )
 include( "tip4serv/init.lua" )
